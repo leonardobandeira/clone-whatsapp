f26463ab64ba33860509d863a0dd551fe1c9d84b
Leonardo Lucena <leonardolucena.cc@gmail.com>
2023-11-05 17:16:51 -0300

    :sparkles: feat: Import new element from to layout

--- src/layouts/RestrictArea.vue
+++ src/layouts/RestrictArea.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page class="container full-height">
     <div>
-
+      <ConversationArea/>
     </div>
 
-    <div class="column" v-if="false">
+    <div class="column" v-if="true">
+      <TopBar :title="'Leonardo Bandeira'"/>
     </div>
     <Empty v-else/>
   </q-page>
@@ -13,10 +14,16 @@
 <script>
 import { defineComponent, ref } from 'vue'
 import Empty from 'src/components/Empty/Index.vue'
+import ConversationArea from 'src/components/ConversationArea/Index.vue'
+import TopBar from 'src/components/TopBar/Index.vue'
 
 export default defineComponent({
   name: 'RestrictArea',
-  components: {Empty},
+  components: {
+    Empty,
+    ConversationArea, 
+    TopBar
+  },
   setup () {
     return {
     }
