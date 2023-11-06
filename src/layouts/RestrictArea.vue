661ccfe452183975bdfac330a1a0b404b4873ee0
Leonardo Lucena <leonardolucena.cc@gmail.com>
2023-11-06 11:16:18 -0300

    :construction: feat: Important new components in the restrict area

--- src/layouts/RestrictArea.vue
+++ src/layouts/RestrictArea.vue
@@ -6,6 +6,8 @@
 
     <div class="column" v-if="true">
       <TopBar :title="'Leonardo Bandeira'"/>
+      <ChatArea />
+      <MessageBar />
     </div>
     <Empty v-else/>
   </q-page>
@@ -16,13 +18,16 @@ import { defineComponent, ref } from 'vue'
 import Empty from 'src/components/Empty/Index.vue'
 import ConversationArea from 'src/components/ConversationArea/Index.vue'
 import TopBar from 'src/components/TopBar/Index.vue'
-
+import ChatArea from 'src/components/ChatArea/Index.vue'
+import MessageBar from 'src/components/MessageBar/Index.vue'
 export default defineComponent({
   name: 'RestrictArea',
   components: {
     Empty,
-    ConversationArea, 
-    TopBar
+    ConversationArea,
+    TopBar,
+    ChatArea,
+    MessageBar
   },
   setup () {
     return {
@@ -35,6 +40,6 @@ export default defineComponent({
 .container {
   display: flex;
   overflow: hidden;
-  min-width: 1200px; 
+  min-width: 1200px;
 }
 </style>
