c66992d762cfb520c63bac574429b3e34ae3695a
Leonardo Lucena <leonardolucena.cc@gmail.com>
2023-12-30 11:49:25 -0300

    :construction: feat: Move RestrictArea to pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'chat', component: () => import('layouts/RestrictArea.vue') }
+      { path: 'chat', component: () => import('pages/RestrictArea.vue') }
     ]
   },
 
